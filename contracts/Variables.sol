011f87c307bc4336ea02f9abdf7c8f8fe58754b0
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-10-20 10:49:46 +0530

    asas

--- contracts/Variables.sol
+++ contracts/Variables.sol
@@ -4,9 +4,31 @@ pragma solidity ^0.8.13;
 contract StateVariable {
     
     string public myState;
-    string public myNum;
+    uint256 public myNum;
 
     string public defaultText = "Hey Default Test";
 
     uint256 public defaultNum = 5;
+
+    bytes public defaultByte = "hey Default";
+    bytes public defaultBytesNo;
+
+    uint256[] public array;
+
+    constructor(string memory _myState, uint256 _myNum) {
+        myState = _myState;
+        myNum = _myNum;
+    }
+}
+
+
+contract LocalVariable {  
+//  Variables that are defined inside the function are called local variable
+    
+    uint256 public myNumber;
+
+    function local() public {
+
+    }
+
 }
