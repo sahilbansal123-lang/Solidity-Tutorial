9ad763beb9683f2dbc4a48302c22c23e6ff4d28e
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-10-20 10:36:41 +0530

    asas

--- contracts/Variables.sol
+++ contracts/Variables.sol
@@ -3,4 +3,10 @@ pragma solidity ^0.8.13;
 
 contract StateVariable {
     
+    string public myState;
+    string public myNum;
+
+    string public defaultText = "Hey Default Test";
+
+    uint256 public defaultNum = 5;
 }
