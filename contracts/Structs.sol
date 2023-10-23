00b6870b410e2b4cfa5797a9bc0b2b4366b72494
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-10-23 10:37:50 +0530

    addsaz

--- contracts/Structs.sol
+++ contracts/Structs.sol
@@ -3,4 +3,32 @@ pragma solidity ^0.8.13;
 
 contract Todos {
     
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    // An array is declerd when defining a struct
+    Todo[] public todos;
+
+    function create(string calldata _text) public {
+        // 3 ways to initialize our struct
+        // -> 1. calling it like a function
+        todos.push(Todo(_text, false));
+
+        // 2. key value mapping
+        todos.push(Todo({text: _text, completed: false}));
+
+        // 3. initialize an empty struct and then update
+        Todo memory todo;
+        todo.text = _text;
+        // todo.completed is initializes with false
+        todos.push(todo);
+
+    }
+
+    function get(uint _index) public view returns(string memory text, bool completed)  {
+        Todo storage todo = todos[_index];
+        return (todo.text, todo.completed);
+    }
 }
