5878c9aaa4c760dc550ee829385c9664a55e2c7c
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-10-24 23:05:33 +0530

    das

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.13;
 
 contract Interfaces {
+
+    uint public count;
+
+    function increment() external {
+        count += 1;
+    }
+}
+
+interface ICounter {
+    
+    function count() external view returns (uint);
     
 }
