69a3e7e76ca5023d318a84a6d221557dab739962
sahilbansal123-lang <bansalsahil9053@gmail.com>
2023-10-24 23:16:26 +0530

    das

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -13,5 +13,27 @@ contract Interfaces {
 interface ICounter {
     
     function count() external view returns (uint);
-    
+
+    function increment() external;
+
 }
+
+contract MyContract {
+    
+    function incrementCounter(address _counter) external {
+        ICounter(_counter).increment();
+    }
+
+    function getCount(address _counter) external view returns(uint) {
+        return ICounter(_counter).count();
+    }
+}
+
+interface UniswapV2Factory {
+    function getPairs(address tokenA, address tokenB) external view returns(address pair);
+}
+
+interface UniSwapV2Pair {
+    function getReserves() external view returns(uint112 reserve0, uint112 reserve1, uint32 blockTimeStamp);
+}
+
